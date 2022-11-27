8e85de2236ec74ca9ccae3502122f35479309425
Neil M <plan.with.neil@gmail.com>
2022-11-26 18:10:11 -0600

    use async return type

--- main.ts
+++ main.ts
@@ -9,10 +9,16 @@ export interface TypeofMap {
   symbol: symbol;
 }
 
-enum ErrorCodes {
+// enum GuardKeys {
+//   _type = '_type',
+//   _code = '_code',
+// }
+
+enum _Codes {
   UNEXPECTED_ERROR = 'UNEXPECTED_ERROR',
   NOT_FOUND = 'NOT_FOUND',
   INVALID_ID = 'INVALID_ID',
+  VALIDATION_ERROR = 'VALIDATION_ERROR',
 }
 
 enum _Types {
@@ -21,7 +27,7 @@ enum _Types {
 
 interface BaseCodedError {
   message: string;
-  _code: ErrorCodes;
+  _code: _Codes;
   _type: _Types.CODED_ERROR;
 }
 
@@ -36,17 +42,23 @@ interface UnexpectedError extends BaseCodedError {
   cause?: Error;
   /** This is the original error if it was not an error instance. */
   unknownCause?: unknown;
-  _code: ErrorCodes.UNEXPECTED_ERROR;
+  _code: _Codes.UNEXPECTED_ERROR;
+}
+
+interface ValidationError extends BaseCodedError {
+  _code: _Codes.VALIDATION_ERROR;
 }
 
 /**
  * Converts an unknown error into a default {@link UnexpectedError} that's easier to work with.
  */
 export const createUnexpectedError = (error: unknown): UnexpectedError => {
+  const defaultMessage = 'There was an unexpected error.';
+
   const unexpectedError: UnexpectedError = {
-    message: 'There was an unexpected error.',
+    message: defaultMessage,
     _type: _Types.CODED_ERROR,
-    _code: ErrorCodes.UNEXPECTED_ERROR,
+    _code: _Codes.UNEXPECTED_ERROR,
   };
 
   if (error instanceof Error) {
@@ -115,20 +127,71 @@ export const isObjectWithKey = <K extends string, TypeofResult extends keyof Typ
   return isNonNullObject(item) && key in item && typeof item[key] === typeofResult;
 };
 
-export const trySync = <OkT>(callBack: () => OkT): OkT | UnexpectedError => {
+const tryFailed = (result: unknown): result is UnexpectedError =>
+  isNonNullObject(result) &&
+  '_type' in result &&
+  result._type === _Types.CODED_ERROR &&
+  '_code' in result &&
+  result._code === _Codes.UNEXPECTED_ERROR;
+
+export const trySync = <Fn extends () => any>(fn: Fn): ReturnType<Fn> | UnexpectedError => {
   try {
-    return callBack();
+    return fn();
   } catch (error) {
     return createUnexpectedError(error);
   }
 };
 
-export const tryAsync = async <OkT>(callBack: () => Promise<OkT>): Promise<OkT | UnexpectedError> => {
+type AsyncReturnType<Fn extends (...args: any) => Promise<any>> = Fn extends (...args: any) => Promise<infer R>
+  ? R
+  : unknown;
+
+/**
+ * Takes an async function that could throw and instead returns the error or fulfilled data.
+ */
+export const tryAsync = async <Fn extends () => Promise<any>>(
+  fn: Fn
+): Promise<UnexpectedError | AsyncReturnType<Fn>> => {
   try {
-    return await callBack();
+    return await fn();
   } catch (error) {
     return createUnexpectedError(error);
   }
+};
+
+interface User {
+  username: string;
+}
+
+const dbCreateUser = async (username: string, shouldError: boolean): Promise<User> => {
+  if (shouldError) {
+    if (Math.random() < 0.5) {
+      const e = new Error('Db: username must be unique');
+      e.name = 'NonUniqueError';
+      throw e;
+    }
+
+    throw new Error('Db: timed out.');
+  }
+  return {
+    username,
+  };
+};
+
+const createUserService = async (username: string): Promise<User | ValidationError | UnexpectedError> => {
+  if (username.length < 3) {
+    return {
+      _code: _Codes.VALIDATION_ERROR,
+      _type: _Types.CODED_ERROR,
+      message: 'Username must be at least 3 characters long.',
+    };
+  }
+
+  const newUserResult = await tryAsync(async () => {
+    return dbCreateUser('test', true);
+  });
+
+  return newUserResult;
 };
 
 // export function fromThrowable<Fn extends (...args: readonly any[]) => any, E>(
